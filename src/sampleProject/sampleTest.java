39cb755a6dce570ef1d7a130102de0ba421d545d
dipesh choudhary <dipsc4@gmail.com>
2024-07-26 14:38:10 +0530

    sampleProject

---
+++ src/sampleProject/sampleTest.java
@@ -0,0 +1,16 @@
+package sampleProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.orangehrm.com/");
+
+	}
+
+}
